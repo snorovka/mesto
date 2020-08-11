5eb131afee0226070fc2e4bce2af15117b64276f
Yulia Zorina <yulia.zorina@yahoo.com>
2020-08-11 11:31:19 +0200

    edited js

--- scripts/main.js
+++ scripts/main.js
@@ -13,27 +13,25 @@ let profileJob = document.querySelector('.profile__job');
 //массив полей ввода popup
 let inputs = document.querySelectorAll('input');
 
-//редактирование полей profile по кнопке "редактировать"
-const popupOpen = function (e) {
-  e.preventDefault();
-  inputs[0].value = profileName.textContent;
-  inputs[1].value = profileJob.textContent;
-  popup.classList.toggle('popup_opened');
-}
-popupEditButton.addEventListener ('click', popupOpen);
-
 //открытие - закрытие popup
 const popupToggle = function (event) {
-  event.preventDefault();
   popup.classList.toggle('popup_opened');
 }
 popupCloseButton.addEventListener ('click', popupToggle);
 
+//редактирование полей profile по кнопке "редактировать"
+const popupOpen = function (e) {
+  inputs[0].value = profileName.textContent;
+  inputs[1].value = profileJob.textContent;
+  popupToggle();
+}
+popupEditButton.addEventListener ('click', popupOpen);
+
 //сохранение полей profile по кнопке "сохранить"
 function formSubmitHandler (evt) {
-    evt.preventDefault();
-    profileName.textContent = inputs[0].value;
-    profileJob.textContent = inputs[1].value;
-    popup.classList.toggle('popup_opened');
+  evt.preventDefault();
+  profileName.textContent = inputs[0].value;
+  profileJob.textContent = inputs[1].value;
+  popupToggle();
 }
 popupSaveButton.addEventListener('click', formSubmitHandler);
