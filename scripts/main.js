bc44205cbfa26992c469f0c58de7fa051deed594
Yulia Zorina <yulia.zorina@yahoo.com>
2020-08-27 09:40:13 +0200

    edit card add-form

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,40 @@
+//массив карточек
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 //переменная всплывающего окна popup
 let popup = document.querySelector('.popup');
 
-//переменные кнопок
+//переменные и константы кнопок
 let popupEditButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popup.querySelector('.popup__close');
 let popupSaveButton = popup.querySelector('.popup__save');
+
+const popupAddButton = popup.querySelector('.profile__add-button');
 
 //переменная формы
 let popupForm = document.querySelector('#profile-edit');
@@ -16,6 +46,12 @@ let profileJob = document.querySelector('.profile__job');
 //переменные полей ввода popup
 let inputName = document.querySelector('#name');
 let inputJob = document.querySelector('#job');
+
+
+//переменные полей карточки card
+const cardName = document.querySelector('#card-name');
+const cardLink = document.querySelector('#card-link');
+
 
 //открытие - закрытие popup
 const popupToggle = function () {
@@ -38,4 +74,16 @@ function formSubmitHandler (evt) {
   profileJob.textContent = inputJob.value;
   popupToggle();
 }
-popupForm.addEventListener('submit', formSubmitHandler);
+popupForm.addEventListener ('submit', formSubmitHandler);
+
+
+//добавление карточки card по кнопке "+"
+//popupAddButton.addEventListener ('click', popupToggle);
+
+//function formSubmitHandler (evt) {
+  //evt.preventDefault();
+  //profileName.textContent = inputName.value;
+  //profileJob.textContent = inputJob.value;
+  //popupToggle();
+//}
+//popupForm.addEventListener('submit', formSubmitHandler);
