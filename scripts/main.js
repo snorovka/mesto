7a58de16ef9ef9fd8c4d57e007849ced78461c95
Yulia Zorina <yulia.zorina@yahoo.com>
2020-10-07 17:56:25 +0200

    creaed english version

--- scripts/main.js
+++ scripts/main.js
@@ -1,120 +1,78 @@
-//массив карточек
+//cards array
 const initialCards = [
   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+      name: 'Greece',
+      link: './images/cards_myrtos.png'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+      name: 'Scotland',
+      link: './images/cards_scotland.png'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+      name: 'Tbilisi',
+      link: './images/cards_tbilisi.png'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+      name: 'Baikal',
+      link: './images/cards_baikal.png'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+      name: 'Altai',
+      link: './images/cards_altai.png'
   },
   {
-      name: 'Воронеж',
-      link: 'https://images.unsplash.com/photo-1567345242672-1c9cd67cb860?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'
+      name: 'Georgia',
+      link: './images/cards_georgia1.png'
   }
 ];
 
-console.log(initialCards);
-
-//контейнер массива карточек
+//cards array
 const initialCardsContainer = document.querySelector(".cards");
-const addCardForm = document.querySelector('#card-add');
 
-//переменные форм popup
-let popupProfile = document.querySelector('#profile-edit');
-let popupCard = document.querySelector('#card-add');
+//popup var
+let popupProfile = document.querySelector('.popup');
 
-//переменные и константы кнопок
+//form var
+let popupForm = document.querySelector('#profile-edit');
+
+//buttons var
 let popupEditButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popupProfile.querySelector('.popup__close');
 let popupSaveButton = popupProfile.querySelector('.popup__save');
 
-const cardAddButton = document.querySelector('.profile__add-button');
-
-//переменные полей profile
+//profile var
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-//переменные полей ввода popup
+//popup input textarea var
 let inputName = document.querySelector('#name');
 let inputJob = document.querySelector('#job');
 
-
-
-//добавление карточек в контейнер
+//add cards from array to container
   const addCardToContainer = (cardItem) => {
   const cardElement = document.querySelector("#cardTemplate").content.cloneNode(true);
 
   cardElement.querySelector(".card__title").textContent = cardItem.name;
   cardElement.querySelector(".card__image").src = cardItem.link;
 
-  //удаление карточки
+  //delete card
   cardElement.querySelector('.card__delete').addEventListener ('click', event => {
     const card = event.target.closest('.card');
     card.remove()
   });
 
-  initialCardsContainer.append(cardElement);
+  initialCardsContainer.prepend(cardElement);
 }
+
 initialCards.forEach(addCardToContainer);
 
-//переменные полей формы добавления карточки card
-//const inputCardName = document.querySelector('#card-name');
-//const inputCardImage = document.querySelector('#card-image');
-
-//функция открытия-закрытие формы popup
-function popupToggle() {
-  popupProfile.classList.toggle('.popup_opened');
+//open-close popup
+  const popupToggle = function () {
+  popupProfile.classList.toggle('popup_opened');
 }
+popupCloseButton.addEventListener ('click', popupToggle);
 
-//popupCloseButton.addEventListener ('click', function() {
-  //popupToggle(popupCard)
-  //});
-
-//открытие формы добавления карточки card по кнопке "+"
-//cardAddButton.addEventListener('click', function() {
-  //popupToggle(popupCard)
-  //});
-
-//const cardAdd = function () {
-  //inputName.value = profileName.textContent;
-  //inputJob.value = profileJob.textContent;
-  //popupToggle();
-//}
-//cardAddButton.addEventListener ('click', cardAdd);
-
-//добавление карточки card по кнопке "+"
-//addCardForm.addEventListener('submit', event => {
-  //event.preventDefault();
-
-  //const cardEl = document.querySelector(".card");
-  //console.log(cardEl);
-
-  //cardEl = addCardForm.querySelector('#card-name').value;
-  //cardEl.link = addCardForm.querySelector('#card-image').value;
-  //console.log(cardEl);
-
-  //addCardToContainer(cardEl);
-
-  //addCardForm.reset();
-  //popupToggle();
-
-//});
-
-
-//редактирование полей profile по кнопке "редактировать"
+//edit profile textareas
 const popupOpen = function () {
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
@@ -122,11 +80,11 @@ const popupOpen = function () {
 }
 popupEditButton.addEventListener ('click', popupOpen);
 
-//сохранение полей profile по кнопке "сохранить"
+//edit profile textareas
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
   popupToggle();
 }
-popupProfile.addEventListener ('submit', formSubmitHandler);
+popupForm.addEventListener('submit', formSubmitHandler);
